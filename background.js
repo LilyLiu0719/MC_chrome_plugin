7bbad94b1290fc722119427cd3da10de6e1b2fd5
LilyLiu0719 <b06901004@ntu.edu.tw>
2022-09-20 18:09:57 +0800

    add findandreplace function

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 chrome.runtime.onInstalled.addListener(async () => {
-	let keys = ['我要變成紅色'];
-	let values = ['我要變成<span style="color: red">'+'紅'+'</span>色'];
-    chrome.storage.sync.set({
-        keys: keys,
-        values: values
-    }, function(result) {
-        console.log(result);
-    });
+	let keys = ['牛肉漢堡', '豬肉漢堡', '比薩', '漢堡', '變長'];
+	let values = ['Beaf Burger', 'Pork Burger', 'Pizza', "🍔", '變長-----------------------------------長'];
+	//let keys = ['(?<=我要變成)紅(?=色)'];
+	//let values = ['我要變成<span style="color: red">'+'紅'+'</span>色'];
+  chrome.storage.sync.set({
+      keys: keys,
+      values: values
+  }, );
 });
 
--- contentScript.js
+++ contentScript.js
@@ -1,45 +1,40 @@
-window.onload = replacer;
 replaceStart();
+window.onload = replacer;
 
 var keys = [] 
 var values = [] 
 
-String.prototype.replaceArray = function(find, replace) {
-  var replaceString = this;
-  var regex; 
-  for (var i = 0; i < find.length; i++) {
-  regex = new RegExp(find[i], "gi");
-  replaceString = replaceString.replace(regex, replace[i]);
-  }
-  return replaceString;
-  };
-
 function replacer() {
-  var elements = document.getElementsByTagName('*');
-  for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-    for (var j = 0; j < element.childNodes.length; j++) {
-      var node = element.childNodes[j];
-      if (node.nodeType == 3) {
-        var text = node.nodeValue;
-        var replacedText = text.replaceArray(keys, values)
-        if (replacedText !== text) {
-          element.innerHTML = replacedText;
-          //element.replaceChild(document.createTextNode(replacedText), node);
-        }
-      }             
-    }
-  }
+	var container = document.body;
+  var find;
+  var target;
+  
+  for (var i = 0; i < keys.length; i++) { 
+    find = keys[i];
+    target = values[i]
+    regex = new RegExp(find, "gi");
+    const instance = findAndReplaceDOMText(container, {
+      preset: 'prose',
+      find: regex,
+      //wrap: 'span', 
+      //wrapClass: 'red', 
+      replace: function(portion) {
+        called = true;
+        console.log(portion);
+        var el = document.createElement('span');
+        el.innerHTML = target;
+        return target;
+      },
+      forceContext: findAndReplaceDOMText.NON_INLINE_PROSE
+    });
+  };
 }
 
 function replaceStart() {
   chrome.storage.sync.get(['keys', 'values'], function(result) {
-  console.log("loaded")
-  console.log(result)
   if (result.keys && result.values) {
     keys = result.keys
     values = result.values
   }
-  console.log('Loading ReplaceR');
   });
 }
